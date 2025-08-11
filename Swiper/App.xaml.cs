8bce02d88bc18feef32c5b26aef6a873f091b1b9
Edanriell <Edanriell@gmail.com>
2025-08-11 17:19:54 +0300

    initial commit

---
+++ Swiper/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace Swiper;
+
+public partial class App : Application
+{
+    public App() { InitializeComponent(); }
+
+    protected override Window CreateWindow(IActivationState? activationState) { return new Window(new AppShell()); }
+}
---
+++ Swiper/AppShell.xaml.cs
@@ -0,0 +1,6 @@
+﻿namespace Swiper;
+
+public partial class AppShell : Shell
+{
+    public AppShell() { InitializeComponent(); }
+}
