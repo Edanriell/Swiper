7832c051b5c69d92f99492808d147caedb5cbbf8
Edanriell <Edanriell@gmail.com>
2025-08-12 14:58:45 +0300

    feature(image-download): image is downloading

--- Swiper/Controls/SwiperControl.xaml.cs
+++ Swiper/Controls/SwiperControl.xaml.cs
@@ -1,6 +1,14 @@
+using Swiper.Utils;
+
 namespace Swiper.Controls;
 
 public partial class SwiperControl : ContentView
 {
-    public SwiperControl() { InitializeComponent(); }
+    public SwiperControl()
+    {
+        InitializeComponent();
+        var picture = new PictureGenerator();
+        descriptionLabel.Text = picture.Description;
+        image.Source = new UriImageSource { Uri = picture.Uri };
+    }
 }
